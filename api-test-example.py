d06d49faa7d0d7ac670274418a5759bfac25cb0b
Alexander Hübler <112554420+alx-hblr@users.noreply.github.com>
2023-03-24 15:51:51 +0100

    Add files via upload

--- api-test-example.py
+++ api-test-example.py
@@ -2,7 +2,7 @@ import unittest
 import requests
 
 class TestRecipeAPI(unittest.TestCase):
-    API_URL = "http://localhost:8080/recipes"
+    API_URL = "http://127.0.0.1:8080/recipes"
 
     def test_get_all_recipes(self):
         response = requests.get(self.API_URL)
