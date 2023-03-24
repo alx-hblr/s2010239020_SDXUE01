8e2d6c94b50c0831e7d22957146f53d85eb55188
Alexander Hübler <112554420+alx-hblr@users.noreply.github.com>
2023-03-24 15:46:09 +0100

    Add files via upload

--- api-test-example.py
+++ api-test-example.py
@@ -8,6 +8,22 @@ class TestRecipeAPI(unittest.TestCase):
         response = requests.get(self.API_URL)
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
+    
+    def test_create_recipe(self):
+        # Define a recipe to create
+        recipe = {
+            "name": "Test Recipe",
+            "description": "This is a test recipe.",
+            "ingredients": ["Ingredient 1", "Ingredient 2"],
+            "instructions": "Test instructions."
+        }
+
+        # Send a POST request to create the recipe
+        response = requests.post(self.API_URL, json=recipe)
+
+        # Check that the response has a success status code and contains the created recipe
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), recipe)
 
 if __name__ == '__main__':
     unittest.main()
